fe9b50774455a4794998baccbef75989eefa2f62
Judy McGoldrick <jjeanius@verizon.net>
2019-01-05 00:32:47 +0000

    Automatically backed up by Learn

--- db/migrate/20190105003118_create_authors.rb
+++ db/migrate/20190105003118_create_authors.rb
@@ -3,7 +3,7 @@ class CreateAuthors < ActiveRecord::Migration[5.0]
     create_table :authors do |t|
       t.string :name
       t.string :genre
-      t.test :bio
+      t.text :bio
 
       t.timestamps
     end
